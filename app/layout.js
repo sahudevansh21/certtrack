12ce5248ac794280c2c61a9bdff090c01ab56a35
Devansh Sahu <sahudevansh21@gmail.com>
2026-06-18 12:45:33 +0530

    Add app/layout.js

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+import './globals.css';
+export default function RootLayout({ children }) {
+  return (
+    <html lang='en'>
+      <head>
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <script src='https://cdn.tailwindcss.com'></script>
+        <title>CertTrack</title>
+      </head>
+      <body className='antialiased'>{children}</body>
+    </html>
+  );
+}
